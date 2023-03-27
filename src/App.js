e411b609900c568fa1a595d0280df1e3a76eeca7
bagdadiemre <bagdadiemre@gmail.com>
2023-03-27 13:22:37 +0300

    style.css added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>0</h1>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  );
 }
 
 export default App;
