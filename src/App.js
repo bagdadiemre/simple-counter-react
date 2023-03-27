1fcfb57e5b75da4830ac010bbcf7ae931e218a87
bagdadiemre <bagdadiemre@gmail.com>
2023-03-27 13:52:22 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import "./styles.css";
+
 function App() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
+  function incrementBy5() {
+    setCount(count + 5);
+  }
+  function decrementBy5() {
+    setCount(count - 5);
+  }
+  function reset() {
+    setCount(0);
+  }
+
   return (
     <div className="App">
-      <h1>0</h1>
-      <button>-</button>
-      <button>+</button>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={reset} disabled={count === 0}>
+          R
+        </button>
+      </div>
+      <div>
+        <button onClick={decrement} disabled={count === 0}>
+          -
+        </button>
+        <button onClick={increment}>+</button>
+      </div>
+      <div>
+        <button onClick={decrementBy5} disabled={count - 5 < 0}>
+          -5
+        </button>
+        <button onClick={incrementBy5}>+5</button>
+      </div>
     </div>
   );
 }
